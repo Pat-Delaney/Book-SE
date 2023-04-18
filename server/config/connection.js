856946138ba018d7d65ed363c39eaa13a50d0c75
Patrick <patrick@Patricks-iMac.local>
2023-04-17 18:43:48 -0700

    debug connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/your-database-name');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/your-database-name');
 
 module.exports = mongoose.connection;
